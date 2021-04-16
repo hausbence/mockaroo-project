7b5e3f1a477ac6347e778fb2f425e24fb94c2582
hausbence <bence.hausknecht@gmail.com>
2021-04-16 16:18:31 +0200

    initalize project

---
+++ src/main/java/com/worldofbooks/mockaroo/MockarooApplication.java
@@ -0,0 +1,13 @@
+package com.worldofbooks.mockaroo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockarooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockarooApplication.class, args);
+    }
+
+}
